5fa833e1b4efd240942b5e40db89921efc9c7778
Carlo Sciolla <skuro@skuro.tk>
2010-04-08 00:30:36 +0200

    Swank server now starts but class loading is not OK yet

--- src/main/java/it/sk/alfresco/clojure/RunSwankServer.java
+++ src/main/java/it/sk/alfresco/clojure/RunSwankServer.java
@@ -12,19 +12,35 @@ import clojure.lang.Compiler;
  * Time: 12:20:19 AM
  * To change this template use File | Settings | File Templates.
  */
-public class RunSwankServer
-{
-    public void init() throws Exception
-    {
-        final String startSwankScript =
-                "(require 'clojure.main)\n" +
-                        "(require 'swank.swank)\n" +
-                        "(clojure.main/with-bindings\n" +
-                        "  (swank/ignore-protocol-version \"2008-11-23\")\n" +
-                        "  (swank/start-server \"nul\" :encoding \"utf-8-unix\" :port 4005))\n";
+public class RunSwankServer {
+    public void init() throws Exception {
+        Runnable cljRunner = new Runnable() {
+            public void run() {
+                Thread thisThread = Thread.currentThread();
+                ClassLoader savedCL = thisThread.getContextClassLoader();
 
+                ClassLoader newCL = this.getClass().getClassLoader();
 
-        Compiler.load(new StringReader(startSwankScript));
+                thisThread.setContextClassLoader(newCL);
+
+                try {
+                    clojure.lang.Compiler.load(
+                            new java.io.StringReader(
+                                    "(require 'clojure.main)\n" +
+                                            "(require 'swank.swank)\n" +
+                                            "(clojure.main/with-bindings\n" +
+                                            "    (swank.swank/start-server \"nul\" :encoding \"utf-8\" :port 4005))"
+                            ));
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                thisThread.setContextClassLoader(savedCL);
+            }
+        };
+
+        Thread cljThread = new Thread(cljRunner);
+        cljThread.start();
 
     }
 }
