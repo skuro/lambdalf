516547dbbcea443ae3ae4f3418d203e67e4ad2da
Carlo Sciolla <skuro@skuro.tk>
2010-04-07 08:49:25 +0200

    Unstable, non working initial code that runs the swank server within jetty

---
+++ src/main/java/it/sk/alfresco/clojure/RunSwankServer.java
@@ -0,0 +1,29 @@
+package it.sk.alfresco.clojure;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import clojure.lang.Compiler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sku
+ * Date: Apr 7, 2010
+ * Time: 12:20:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RunSwankServer
+{
+    public void init ()  throws Exception
+    {
+        final String startSwankScript =
+            "(add-classpath \"file:///c:/usr/unpacked/clojure/swank-clojure/\")\n" +
+            "(require (quote swank) (quote clojure.main))\n" +
+            "(clojure.main/with-bindings\n" +
+            "  (swank/ignore-protocol-version \"2008-11-23\")\n" +
+            "  (swank/start-server \"nul\" :encoding \"utf-8-unix\" :port 4005))\n";
+
+
+        Compiler.load(new StringReader(startSwankScript));
+        
+    }
+}
