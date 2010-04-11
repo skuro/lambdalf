a982a0ec18f5ef8dc2c1a388d943f880eda6d19f
Carlo Sciolla <carlo.sciolla@gmail.com>
2010-04-11 22:59:51 +0200

    The swank server starts OK, the it.sk.alfresco clojure namespace looks like not exported/visible through slime

--- src/main/clojure/it/sk/alfresco/core.clj
+++ src/main/clojure/it/sk/alfresco/core.clj
@@ -1,11 +1,16 @@
 (ns it.sk.alfresco
-;    (:require (swank.swank))
+    (:require (swank.swank)
+              (clojure.main))
     (:import (it.sk.alfresco.clojure ContextHelper)
              (org.alfresco.service ServiceRegistry)))
 
-;(defonce *services*
-;    (. (ContextHelper/getApplicationContext) getBean ServiceRegistry/SERVICE_REGISTRY))
+(defonce *services*
+    (. (ContextHelper/getApplicationContext) getBean ServiceRegistry/SERVICE_REGISTRY))
+
+;(defn start-swank []
+;    (clojure.main/with-bindings
+;        (swank.swank/create-server "/dev/null" :port 4005 :dont-close true)))
 
 (defn start-swank []
-    (clojure.main/with-bindings
-        (swank.swank/start-server "/dev/null" :port 4005 :dont-close true)))
+  (clojure.main/with-bindings
+    (swank.swank/start-server "nul" :encoding "utf-8" :port 4005)))
--- src/main/java/it/sk/alfresco/clojure/RunSwankServer.java
+++ src/main/java/it/sk/alfresco/clojure/RunSwankServer.java
@@ -3,14 +3,10 @@ package it.sk.alfresco.clojure;
 /**
  * @author Carlo Sciolla
  */
-public class RunSwankServer
-{
-    public void init() throws Exception
-    {
-        Runnable cljRunner = new Runnable()
-        {
-            public void run()
-            {
+public class RunSwankServer {
+    public void init() throws Exception {
+        Runnable cljRunner = new Runnable() {
+            public void run() {
                 Thread thisThread = Thread.currentThread();
                 ClassLoader savedCL = thisThread.getContextClassLoader();
 
@@ -18,16 +14,15 @@ public class RunSwankServer
 
                 thisThread.setContextClassLoader(newCL);
 
-                try
-                {
+                try {
                     clojure.lang.Compiler.load(
                             new java.io.StringReader(
-                                    "(require '(it.sk.alfresco core))\n" +
-                                            "(start-swank)"
+                                    "(require 'clojure.main)\n" +
+                                            "(require 'swank.swank)\n" +
+                                            "(clojure.main/with-bindings\n" +
+                                            "    (swank.swank/start-server \"nul\" :encoding \"utf-8\" :port 4005))"
                             ));
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
