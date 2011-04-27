dc86cf7b5f90a295eaddacf6b0f4eb2cb2298246
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-04-27 17:09:03 +0200

    Refactoring, cleanup

--- src/main/clojure/alfresco/core.clj
+++ src/main/clojure/alfresco/core.clj
@@ -7,11 +7,14 @@
 (defrecord SimpleNode
   [id store])
 
-(defonce *alfresco-services*
-    (. (ContextHelper/getApplicationContext) getBean ServiceRegistry/SERVICE_REGISTRY))
-
 (defprotocol Clojurify
   "Transform Java object into Clojure entities anc vice versa.
    Used to hide any Java to the final user"
   (j2c [this] "Creates a Clojure representation of the given object")
   (c2j [this] "Creates a Java representation of the given clojure entity"))
+
+(defonce *alfresco-services*
+  (. (ContextHelper/getApplicationContext) getBean ServiceRegistry/SERVICE_REGISTRY))
+
+(defmacro behave-on
+  "Registers a new behavior to react upon the provided ")
