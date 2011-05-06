afc7e0171ab1b76b1a18a90cd3e0b945697c26b4
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-05-06 23:22:43 +0200

    added StreamContent extension

---
+++ lambdalf-lib/src/main/clojure/alfresco/interop.clj
@@ -0,0 +1,14 @@
+(ns alfresco.interop
+  (:import [org.alfresco.repo.web.scripts.content StreamContent]
+           [org.springframework.extensions.webscripts WebScriptRequest WebScriptResponse])
+  (:require [alfresco.auth :as a]))
+
+; Allow content to be streamed back bypassing permission checks
+(gen-class :name alfresco.interop.StreamContent
+           :prefix "stream-"
+           :extends org.alfresco.repo.web.scripts.content.StreamContent
+           :exposes-methods {execute executeSuper})
+
+(defn stream-execute
+   [this ^WebScriptRequest req ^WebScriptResponse res]
+   (a/as-admin (.executeSuper this req res)))
