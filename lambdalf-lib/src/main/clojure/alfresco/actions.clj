ed840df897d3908ca0266f26230c72f1f36f1bad
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-05-15 16:37:32 +0200

    Towards real support for actions

--- lambdalf-lib/src/main/clojure/alfresco/actions.clj
+++ lambdalf-lib/src/main/clojure/alfresco/actions.clj
@@ -1,20 +1,55 @@
 (ns alfresco.actions
-  (:require [alfresco.core :as c])
-  (:import [org.alfresco.service.cmr.action Action]
-           [org.alfresco.service.cmr.repository NodeRef]))
+  (:use [clojure.string])
+  (:require [alfresco.core :as c]
+            [alfresco.model :as m])
+  (:import [org.alfresco.repo.action ParameterDefinitionImpl]
+           [org.alfresco.service.cmr.repository NodeRef]
+           [java.util List]))
 
-(gen-class :name alfresco.actions.Action
+(defprotocol Action
+  "All the abtract methods an Action needs to define in Alfresco"
+  (needs-params [this] "Returns a seq of parameters this Action understands")
+  (exec [this action node] "Call the concrete action implementation, providing an action descriptor and the actioned upone node"))
+
+(defrecord Param
+  [name type multi label mandatory])
+
+(extend-protocol c/Clojurify
+  Param
+  (c2j [^Param p]
+       (ParameterDefinitionImpl. (:name p)
+                                 (m/qname (:type p))
+                                 (:mandatory p)
+                                 (:label p))))
+
+(defn- translate-params
+  "Handles the Clojure / Java impedance mismatch when it comes to action parameters"
+  [^Action action ^List current-params]
+  (let [java-params (map c/c2j (needs-params action))]
+    (map #(.add current-params %) java-params)))
+
+;; Interop: allows the Clojure action to be registered as a Spring bean
+(gen-class :name alfresco.actions.ActionRegistrar
            :extends  org.alfresco.repo.action.executer.ActionExecuterAbstractBase
            :prefix "act-"
-           :constructors {[String String] []} ; ns + fn name
-           :init init)
+           :constructors {[String] []} ; FQN of the concrete Action
+           :init clojure-init)
 
-(defn act-init
-  [^String namespace ^String func]
-  (require (symbol namespace))
-  [[] (symbol (str namespace "/" func))])
+(defn act-clojure-init
+  "Stores an instance of the supplied concrete Action as a state"
+  [^String impl]
+  (let [[namespace func] (map symbol (split impl #"/"))]
+    (require namespace)
+    [[] (eval (list (symbol (str impl "."))))])) ;; dirty reflection hack
 
 (defn act-execute-impl
+  "Passes the ball to the concrete Action"
   [this action node]
-  (let [func (.state this)]
-    (func action (c/j2c node))))
+  (let [impl (.state this)]
+    (exec impl action (c/j2c node))))
+
+(defn act-add-parameter-definitions
+  "Sets the parameters needed by our actions"
+  [this ^List params]
+  (let [impl (.state this)]
+    (translate-params (needs-params impl) params)))
