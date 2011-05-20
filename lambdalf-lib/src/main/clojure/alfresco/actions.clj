e3e9a9d0ba4ce44443a410cb6e3ffb1c5e1ee1d8
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-05-20 18:12:19 +0200

    towards a working action parameter support

--- lambdalf-lib/src/main/clojure/alfresco/actions.clj
+++ lambdalf-lib/src/main/clojure/alfresco/actions.clj
@@ -11,43 +11,35 @@
   (needs-params [this] "Returns a seq of parameters this Action understands")
   (exec [this action node] "Call the concrete action implementation, providing an action descriptor and the actioned upone node"))
 
-(defrecord Param
-  [name type multi label mandatory])
-
-(extend-protocol c/Clojurify
-  Param
-  (c2j [^Param p]
-       (ParameterDefinitionImpl. (:name p)
-                                 (m/qname (:type p))
-                                 (:mandatory p)
-                                 (:label p))))
-
-(defn- translate-params
-  "Handles the Clojure / Java impedance mismatch when it comes to action parameters"
-  [new-params ^List current-params]
-  (let [java-params (map c/c2j new-params)]
-    (map #(.add current-params %) java-params)))
+(defn param
+  "Constructs a Java representation of an action parameter"
+  [{:keys [name type mandatory multi label]}]
+  (ParameterDefinitionImpl. name
+                            (m/qname type)
+                            mandatory
+                            label
+                            multi))
 
 ;; Interop: allows the Clojure action to be registered as a Spring bean
 (gen-class :name alfresco.actions.ActionRegistrar
            :extends  org.alfresco.repo.action.executer.ActionExecuterAbstractBase
            :prefix "act-"
+           :state concrete
            :constructors {[String] []} ; FQN of the concrete Action
-           :init clojure-init) ; base class already has a 'init' method
+           :init clojure-init)         ; base class already has a 'init' method
 
 (defn act-clojure-init
   "Stores an instance of the supplied concrete Action as a state"
   [^String impl]
-  (let [concrete (symbol impl)]
-    [[] (eval (list (symbol (str concrete "."))))])) ;; dirty reflection hack
+  [[] (eval (list (symbol (str impl "."))))]) ; dirty reflection hack
 
 (defn act-executeImpl
   "Passes the ball to the concrete Action"
   [this action node]
-  (let [impl (.state this)]
+  (let [impl (.concrete this)]
     (exec impl action (c/j2c node))))
 
 (defn act-addParameterDefinitions
   "Passes the ball to the concrete Action"
-  [this params]
-  (println "test"))
+  [this ^List params]
+  (map #(.add params %) (needs-params (.concrete this))))
