d329dd1caf9fb4bfac5c2b668c9ca2b7633b868d
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-10-04 19:49:31 +0200

    Initial implementation of tree-seq

---
+++ lambdalf-lib/src/main/clojure/alfresco/seq.clj
@@ -0,0 +1,18 @@
+(ns alfresco.seq
+  (:require [alfresco.nodes :as n]
+            [alfresco.model :as m]
+            [alfresco.auth :as a]))
+
+(defn ts
+  "Returns a lazy seq of the nodes representing the repository branch
+having the given root. Uses the currently authenticated user to realize
+the seq." 
+  [root]
+  ;; store the currently authenticated user, needed by the following closures
+  (let [user (a/whoami)
+        branch? (fn [x] (a/run-as user
+                                 (m/qname-isa? (n/type-qname x)
+                                               (m/qname "cm:folder"))))
+        children (fn [x] (a/run-as user
+                                  (n/children x)))]
+    (tree-seq branch? children root)))
